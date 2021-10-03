e755e7d5380140fbed57ca4ad4c062566e0f391a
Neev100 <79758801+Neev100@users.noreply.github.com>
2021-10-03 16:09:19 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ rightWristY = 0;
 
 function preload() {
     song1 = loadSound("music.mp3");
-    song2 = loadSound("music2.mp3")
+    song2 = loadSound("music2.mp3");
 }
 function setup() {
     canvas = createCanvas(600,500);
